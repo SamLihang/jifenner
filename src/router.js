67731173faf752f33ecc56b2a663f30f6300fa8f
Samlihang <316461408@qq.com>
2019-08-28 18:45:05 +0800

    hange

--- src/router.js
+++ src/router.js
@@ -1,98 +1,115 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Index from './views/index.vue'
-Vue.use(Router)
+import Vue from "vue";
+import Router from "vue-router";
+import Index from "./views/index.vue";
+Vue.use(Router);
 export default new Router({
-  mode: 'hash',
+  mode: "hash",
   base: process.env.BASE_URL,
   routes: [
     {
       path: "/",
-      redirect: '/login',
+      // redirect: '/login',
+      name: "home",
+      component: Index,
+      meta: { title: "" }
     },
     {
-      path: '/login',
-      name: 'login',
-      component: () => import(/* webpackChunkName: "login" */ './views/Login/view.vue'),
-      meta: { title: '建面惠' }
+      path: "/login",
+      name: "login",
+      component: () =>
+        import(/* webpackChunkName: "login" */ "./views/Login/view.vue"),
+      meta: { title: "建面惠" }
     },
     {
-      path: '/home',
-      name: 'home',
-      component: () => import(/* webpackChunkName: "about" */ './views/ShopHome/view.vue'),
-      meta: { title: '建面惠' }
+      path: "/home",
+      name: "home",
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/ShopHome/view.vue"),
+      meta: { title: "建面惠" }
     },
     {
-      path: '/detail',
-      name: 'detail',
-      component: () => import(/* webpackChunkName: "about" */ './views/ShopDetail/view.vue'),
-      meta: { title: '建面惠' }
+      path: "/detail",
+      name: "detail",
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/ShopDetail/view.vue"),
+      meta: { title: "建面惠" }
     },
     {
-      path: '/exchangeResult',
-      name: 'exchangeResult',
-      component: () => import(/* webpackChunkName: "about" */ './views/ExchangeResult/view.vue'),
-      meta: { title: '建面惠' }
+      path: "/exchangeResult",
+      name: "exchangeResult",
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/ExchangeResult/view.vue"),
+      meta: { title: "建面惠" }
     },
     {
-      path: '/integralDetail',
-      name: 'integralDetail',
-      component: () => import(/* webpackChunkName: "about" */ './views//IntegralDetail/view.vue'),
-      meta: { title: '建面惠' }
+      path: "/integralDetail",
+      name: "integralDetail",
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views//IntegralDetail/view.vue"),
+      meta: { title: "建面惠" }
     },
     {
-      path: '/share',
-      name: 'share',
-      component: () => import(/* webpackChunkName: "share") */ './views/share.vue'),
-      meta: { title: '建面惠' }
+      path: "/share",
+      name: "share",
+      component: () =>
+        import(/* webpackChunkName: "share") */ "./views/share.vue"),
+      meta: { title: "建面惠" }
     },
     {
-      path: '/haibao',
-      name: 'haibao',
-      component: () => import(/* webpackChunkName: "haibao") */ './views/haibao.vue'),
-      meta: { title: '建面惠' }
+      path: "/haibao",
+      name: "haibao",
+      component: () =>
+        import(/* webpackChunkName: "haibao") */ "./views/haibao.vue"),
+      meta: { title: "建面惠" }
     },
     {
-      path: '/flag/home',
+      path: "/flag/home",
       name: "flagHome",
-      component: () => import(/* webpackChunkName: 'flaghome' */ './views/flag/home.vue'),
-      meta: { title: 'flag当立' }
+      component: () =>
+        import(/* webpackChunkName: 'flaghome' */ "./views/flag/home.vue"),
+      meta: { title: "flag当立" }
     },
     {
-      path: '/flag/login',
+      path: "/flag/login",
       name: "flagLogin",
-      component: () => import(/* webpackChunkName: 'flaglogin' */ './views/flag/login.vue'),
-      meta: { title: 'flag当立' }
+      component: () =>
+        import(/* webpackChunkName: 'flaglogin' */ "./views/flag/login.vue"),
+      meta: { title: "flag当立" }
     },
     {
-      path: '/flag/upload',
+      path: "/flag/upload",
       name: "flagUpload",
-      component: () => import(/* webpackChunkName: 'flagupload' */ './views/flag/upload.vue'),
-      meta: { title: 'flag当立' }
+      component: () =>
+        import(/* webpackChunkName: 'flagupload' */ "./views/flag/upload.vue"),
+      meta: { title: "flag当立" }
     },
     {
-      path: '/flag/rank',
+      path: "/flag/rank",
       name: "flagRank",
-      component: () => import(/* webpackChunkName: 'flagrank' */ './views/flag/rank.vue'),
-      meta: { title: 'flag当立' }
+      component: () =>
+        import(/* webpackChunkName: 'flagrank' */ "./views/flag/rank.vue"),
+      meta: { title: "flag当立" }
     },
     {
-      path: '/flag/tiyan',
+      path: "/flag/tiyan",
       name: "flagTiyan",
-      component: () => import(/* webpackChunkName: 'flagtiyan' */ './views/flag/tiyan.vue'),
-      meta: { title: 'flag当立' }
+      component: () =>
+        import(/* webpackChunkName: 'flagtiyan' */ "./views/flag/tiyan.vue"),
+      meta: { title: "flag当立" }
     },
     {
-      path: '/flag/share',
+      path: "/flag/share",
       name: "flagshare",
-      component: () => import(/* webpackChunkName: 'flagshare' */ './views/flag/share.vue'),
-      meta: { title: 'flag当立' }
-    }],
+      component: () =>
+        import(/* webpackChunkName: 'flagshare' */ "./views/flag/share.vue"),
+      meta: { title: "flag当立" }
+    }
+  ],
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
-      return savedPosition
+      return savedPosition;
     } else {
-      return { x: 0, y: 0 }
+      return { x: 0, y: 0 };
     }
   }
-})
+});
